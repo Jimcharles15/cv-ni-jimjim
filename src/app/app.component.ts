8f6214109d6a0e4ff8e409f829950c7301d8c6ce
Jimcharles15 <100080305+Jimcharles15@users.noreply.github.com>
2023-01-24 23:23:10 +0800

    4 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,6 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
-  //Interpolation
-  appName = ' This is a One-way Data binding';
-
-  //Property Binding
-  clientName: string = 'Allen';
-
-  //STYLE BINDING, CSS BINDING
-  appliedCSSClass = 'green';
-  notappliedCSSClass = true;
-  myColor = 'red';
-
-  // for two-wy data binding demo
-  myName = '';
-
-  // Event Binding
-  showData($event: any) {
-    console.log('button is clicked!');
-    if ($event) {
-      console.log($event.target);
-      console.log($event.target.value);
-    }
-  }
+  appName = 'This is a One-way Data binding';
+  clientName: string = 'Jimjim';
 }
